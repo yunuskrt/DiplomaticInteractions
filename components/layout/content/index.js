77419bfad64d1cf167b37d8a1917ef736d9989c8
Yunus <ykerestecioglu@sabanciuniv.edu>
2024-06-06 13:54:20 +0300

    removed next-javascript project

--- components/layout/content/index.js
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react'
-import styles from './content.module.css'
-
-const Content = ({ children }) => {
-	return <div className={styles.contentContainer}>{children}</div>
-}
-
-export default Content
